39ee838653cca53481ebcf61a8bd510326994408
Crypto-Hamster <arkadiusz.budkowski.dev@gmail.com>
2021-06-05 11:21:22 +0200

    PrimeT done

--- SPOJ/main.cpp
+++ SPOJ/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 
 int main() {
-
-	std::cout << "Hello, world" << std::endl;
 	
+	int count = 0;
+	int k;
+	int numLoop;
+	std::cin >> numLoop;
+	for (int i = 0; i < numLoop; i++) {
+		std::cin >> k;
+		if (k == 1) {
+			std::cout << "NIE" << std::endl;
+			continue;
+		}
+		for (int j = 1; j < k + 1; j++) {
+			int c = k % j;
+			if (c == 0) {
+				count++;
+			}
+			if (count > 2) {
+				std::cout << "NIE" << std::endl;
+				break;
+			}
+		}
+		if (count <= 2) {
+			std::cout << "TAK" << std::endl;
+		}
+		count = 0;
+	}
 }
